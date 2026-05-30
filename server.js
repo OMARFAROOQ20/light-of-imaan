3ce4e4335a9d15bd6bc0c14f020c93aaec7ff2ac
OMARFAROOQ20 <omarfarooqsk2004@gmail.com>
2026-05-30 20:40:18 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+const prayerRoute = require("./routes/prayer");
+const quranRoute = require("./routes/quran");
+
+app.use("/prayer", prayerRoute);
+app.use("/quran", quranRoute);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log("Server running");
+});
